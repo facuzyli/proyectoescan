99535a7de5551db6d26ee5de53acfe5e0d777793
Facundo Zylinski <fZylinski@dikter.com.ar>
2023-09-05 21:03:16 -0300

    Mi segundo commit

--- src/Scanner.js
+++ src/Scanner.js
@@ -1,39 +1,55 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import Quagga from 'quagga';
 
 function Scanner({ onScan }) {
   const scannerRef = useRef(null);
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    Quagga.init({
-      inputStream: {
-        type: 'LiveStream',
-        constraints: {
-          width: 640,
-          height: 480,
-          facingMode: 'environment'
-        },
-        target: scannerRef.current
-      },
-      decoder: {
-        readers: ['code_39_extended_reader']
-      }
-    }, (err) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      Quagga.start();
-    });
+    setIsMounted(true);
 
-    Quagga.onDetected((data) => {
-      onScan(data.codeResult.code);
-    });
+    return () => setIsMounted(false);
+  }, []);
+
+  useEffect(() => {
+    if (!isMounted) return;
+
+    const initializeQuagga = () => {
+      if (!scannerRef.current) return;
+
+      Quagga.init({
+        inputStream: {
+          type: 'LiveStream',
+          constraints: {
+            width: 640,
+            height: 480,
+            facingMode: 'environment'
+          },
+          target: scannerRef.current
+        },
+        decoder: {
+          readers: ['code_39_extended_reader']
+        }
+      }, (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        Quagga.start();
+      });
+
+      Quagga.onDetected((data) => {
+        onScan(data.codeResult.code);
+      });
+    };
+
+    const timeoutId = setTimeout(initializeQuagga, 500);
 
     return () => {
+      clearTimeout(timeoutId);
       Quagga.stop();
     };
-  }, [onScan]);
+  }, [onScan, isMounted]);
 
   return <div ref={scannerRef} />;
 }
